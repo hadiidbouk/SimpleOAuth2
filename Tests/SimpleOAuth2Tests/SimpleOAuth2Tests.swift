1ff181591f4244bdefc38ebcfb0ebbe53e171dc4
Hadi Dbouk <hadi.dbouk@snow.dog>
2020-12-06 23:33:47 +0200

    Remove tests for now

--- Tests/SimpleOAuth2Tests/SimpleOAuth2Tests.swift
+++ Tests/SimpleOAuth2Tests/SimpleOAuth2Tests.swift
@@ -2,14 +2,5 @@ import XCTest
 @testable import SimpleOAuth2
 
 final class SimpleOAuth2Tests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SimpleOAuth2().text, "Hello, World!")
-    }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
