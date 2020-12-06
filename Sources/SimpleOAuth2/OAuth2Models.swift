0d7d272c16673aaae792d9870261f0e0904e829f
Hadi Dbouk <hadi.dbouk@snow.dog>
2020-12-06 22:25:33 +0200

    Use RFC 6749 OAuth 2.0 access token response

--- Sources/SimpleOAuth2/OAuth2Models.swift
+++ Sources/SimpleOAuth2/OAuth2Models.swift
@@ -7,13 +7,48 @@
 
 import Foundation
 
+/// The Access Token Response
+/// The only required property is the accessToken other properties are optional
+/// Following the RFC 6749 OAuth 2.0 https://tools.ietf.org/html/rfc6749#section-5.1
 public struct OAuth2Credentials: Codable {
     let accessToken: String
-    let tokenType: String
-    let refreshToken: String
-    let scope: String
-    let createdAt: Int
-    let idToken: String
+    let tokenType: String?
+    let refreshToken: String?
+    let scope: String?
+    let expiresIn: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case accessToken
+        case tokenType
+        case refreshToken
+        case scope
+        case expiresIn, expires
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        accessToken = try container.decode(String.self, forKey: .accessToken)
+        tokenType = try? container.decode(String.self, forKey: .tokenType)
+        refreshToken = try? container.decode(String.self, forKey: .refreshToken)
+        scope = try? container.decode(String.self, forKey: .scope)
+
+        var expiresIn: Int?
+        if let expires = try? container.decode(Int.self, forKey: .expires) {
+           expiresIn = expires
+        } else if let expires = try? container.decode(Int.self, forKey: .expiresIn) {
+            expiresIn = expires
+        }
+        self.expiresIn = expiresIn
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(accessToken, forKey: .accessToken)
+        try? container.encode(tokenType, forKey: .tokenType)
+        try? container.encode(refreshToken, forKey: .refreshToken)
+        try? container.encode(scope, forKey: .scope)
+        try? container.encode(expiresIn, forKey: .expiresIn)
+    }
 }
 
 public struct OAuth2Request {
